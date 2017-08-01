701ae4badb6cf1c4f26b600c711d75a971d8b646
crisgon <kristiano.gon@gmail.com>
2017-07-31 23:10:40 -0300

    Arquivo js para add tarefas.[Finalizado]

---
+++ js/adicionaTarefa.js
@@ -0,0 +1,39 @@
+var botaoAddTarefa = document.querySelector(".botaoAddTarefa");
+var tarefaNome = document.querySelector(".tarefaNome");
+var listaDeTarefas = document.querySelector(".listaDeTarefas");
+var liItemTarefa = document.querySelector(".item-tarefa");
+console.log(listaDeTarefas);
+
+botaoAddTarefa.addEventListener("click", function(){
+	
+	criaElementos();
+	addClasses();
+	novoSpanTarefa.textContent = tarefaNome.value; //Pega o valor digitado no input e add no span 
+	montaLi();
+	
+	
+	
+
+
+	tarefaNome.value = "";//Deixa o form vazio
+});
+
+
+function criaElementos(){
+	novoSpanTarefa = document.createElement("span"); //Cria o Span que recebe o nome da tarefa
+	novoLinkDelete = document.createElement("a");//Cria a para deletar
+	novoIconeDelete = document.createElement("i");//Cria novo i para que contem o icone de deletar
+	novoLiTarefa = document.createElement("li"); //Cria o Li que armazena a tarefa
+}
+
+function addClasses(){
+	novoIconeDelete.classList.add("fa", "fa-trash");//Add classes ao icone
+	novoLiTarefa.classList.add("item-tarefa");//Add class dos "lis" ao novo li criado
+}
+
+function montaLi(){
+	novoLinkDelete.appendChild(novoIconeDelete);//Coloca o icone dentro o link a	
+	novoLiTarefa.appendChild(novoSpanTarefa); //Coloca o span como filho do li
+	novoLiTarefa.appendChild(novoLinkDelete);//Coloca o a como filho do li
+	listaDeTarefas.appendChild(novoLiTarefa);//Coloca o li como filho do ul
+}
