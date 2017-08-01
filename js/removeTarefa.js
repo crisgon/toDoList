87bca5e382a8931e8925d9b96a3c3d651cbbe601
crisgon <kristiano.gon@gmail.com>
2017-08-01 00:55:37 -0300

    Remover tarefas[Finalizado].

--- js/removeTarefa.js
+++ js/removeTarefa.js
@@ -2,7 +2,7 @@ var removerTarefa = document.querySelectorAll(".removerTarefa");
 apagaTarefa();
 
 function apagaTarefa(){
-	removerTarefa.forEach(function(indice){
+	removerTarefa.forEach(function(indice){ //Loop para add o remove para cada <a> da lista
 		indice.addEventListener("click", function(event){
 			event.preventDefault();
 			var paiDoElemento = this.parentNode; //Descobre qual é o pai do elemento clicado no evento
